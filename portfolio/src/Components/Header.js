cd82014196dda081878b22b01de8d264a2bf4027
jfi074 <134646835+jfi074@users.noreply.github.com>
2023-08-10 10:27:30 -0400

    Update Header.js

--- portfolio/src/Components/Header.js
+++ portfolio/src/Components/Header.js
@@ -1,13 +1,22 @@
+import Menu from "./Menu";
+import Image from "next/image";
+import Link from "next/link";
 
-import Menu from './Menu'
-function Header({setPage}) {
-    const titre = 'Portfolio';
+import logo from '../public/logo.png'
 
-    return <header >
-       
-     <a href='#' onClick={() => setPage('Accueil')}>
-        <h1 >{titre}</h1>
-     </a>
-        <Menu setPage={setPage}/>
+import styles from '../styles/Header.module.css';
+
+export default function Header() {
+    return <header className={styles.header}>
+        <div className="wrapper">
+        <Link href="/">
+                <Image src={logo} alt="Logo"  width={120} height={150}  />
+            </Link>
+            <div>
+                <h1>Portfolio de Jean fidele</h1>
+            </div>
+           
+            <Menu />
+        </div>
     </header>
 }
